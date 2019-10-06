884cc9be9639dd032b78ae47d91a49addfcddaf6
hyderjacko <hyderjacko.pagano.1737623@studenti.uniroma1.it>
2019-10-06 17:17:45 +0200

    scritta prima bozza semOpen

--- disastrOS_globals.h
+++ disastrOS_globals.h
@@ -17,6 +17,8 @@ extern ListHead resources_list; // resources
 extern ListHead timer_list;     //timers (concrete)
 extern volatile int disastrOS_time; //global time of disastrOS
 
+extern SemaphoreList semaphores_list; //list of all semaphores
+
 // a resource can be a device, a file or an ipc thing
 
 typedef void(*SyscallFunctionType)();
--- disastrOS_semopen.c
+++ disastrOS_semopen.c
@@ -8,4 +8,31 @@
 
 void internal_semOpen(){
   // do stuff :)
+	
+	// We take id semaphore from current process
+	int id = running->syscall_args[0];
+	if(id < 0){
+		printf("ERROR! Negative id, cannot initialized semaphore!\n");
+		running->syscall_retvalue = -1;
+		return;
+	}
+	
+	//We take processes counter from current process
+	int count = running->syscall_args[1];
+	
+	//Checking if semaphore exists already (using SemaphoreList_byId)
+	//*** aggiunta variabile semaphores_list in disastrOS_globals.h ***
+	Semaphore* sem = SemaphoreList_byId(&semaphores_list, id);
+	if(!sem){
+		//Allocating semaphore and add it in semaphores list
+		sem = Semaphore_alloc(id, count);
+		List_insert(&semaphores_list, semaphores_list->last, sem);
+	}
+	
+	//Allocating semaphore descriptor for current process
+	SemDescriptor* sem_dscr = SemDescriptor_alloc(running->last_sem_fd, sem, running);
+	
+	//Set return value of the process to the semaphore file descriptor
+	running->syscall_retvalue = sem_dscr->fd;
+	return;
 }
