2c69ee8a097b7eccf6bf0df77a80dbe27d7b4b47
hyderjacko <hyderjacko.pagano.1737623@studenti.uniroma1.it>
2019-10-07 12:21:38 +0200

    scritta prima bozza semPost

--- disastrOS_sempost.c
+++ disastrOS_sempost.c
@@ -8,4 +8,21 @@
 
 void internal_semPost(){
   // do stuff :)
+	//We take semaphore descriptor from file descriptor,
+	//than we take the semaphore
+	int fd = running->syscall_args[0];
+	SemDescriptor* sem_dscr = SemDescriptorList_byFd(&running->sem_descriptors, fd);
+	Semaphore* sem = sem_dscr->semaphore;
+	
+	//Now we increase semaphore counter
+	//and check if it's <= 0
+	sem->count++;
+	if(sem->count <= 0){
+		//we put the current waiting process in the ready list
+		List_insert(&ready_list, &ready_list->last, (ListItem*)running);
+		running->status = Ready;
+	}
+	
+	running->syscall_retvalue = 0;
+	return;
 }
